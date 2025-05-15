acddab50be7999a14ad783b637fb2d2bd2b3d479
ninadimplere <ninad.s@impleretech.com>
2025-05-15 17:17:03 +0530

    Adds the tabs to the product management page

--- src/app/product-management/page.tsx
+++ src/app/product-management/page.tsx
@@ -2,23 +2,56 @@
 
 import Breadcrumb from "@components/Breadcrumbs/Breadcrumb";
 import { Tabs, Tab } from "@heroui/tabs";
+import { useState } from "react";
 
 export default function Page() {
+  const [activeTab, setActiveTab] = useState<any>("allproducts");
+  const [productCounts, setProductCounts] = useState({
+    allProducts: 0,
+    activeProducts: 0,
+    inactiveProducts: 0,
+    draftProducts: 0,
+    outOfStockProducts: 0,
+    lowStockProducts: 0,
+  });
   return (
     <div className="w-full">
       <Breadcrumb pageName="Product Management" />
 
-      <div className="overflow-hidden rounded-[10px] bg-white shadow-1 dark:bg-gray-dark dark:shadow-card">
-        <div className="flex w-full flex-col">
+      <div className="flex max-h-screen w-full flex-col items-center overflow-hidden rounded-[10px] bg-white py-2 shadow-1 dark:bg-gray-dark dark:shadow-card">
+        <div className="flex">
           <Tabs
-            color="default"
+            color="primary"
             aria-label="Options"
-            selectedKey={"photos"}
-            className="w-full bg-none"
+            selectedKey={activeTab}
+            onSelectionChange={setActiveTab}
+            className="w-full"
+            size="lg"
           >
-            <Tab key="photos" title="Photos"></Tab>
-            <Tab key="music" title="Music"></Tab>
-            <Tab key="videos" title="Videos"></Tab>
+            <Tab
+              key="allproducts"
+              title={`All Products (${productCounts.allProducts})`}
+            ></Tab>
+            <Tab
+              key="activeproducts"
+              title={`Active Products (${productCounts.activeProducts})`}
+            ></Tab>
+            <Tab
+              key="inactiveproducts"
+              title={`Inactive Products (${productCounts.inactiveProducts})`}
+            ></Tab>
+            <Tab
+              key="draftproducts"
+              title={`Products in Draft (${productCounts.draftProducts})`}
+            ></Tab>
+            <Tab
+              key="outofstockproducts"
+              title={`Out of Stock (${productCounts.outOfStockProducts})`}
+            ></Tab>
+            <Tab
+              key="lowstockproducts"
+              title={`Low Stock (${productCounts.lowStockProducts})`}
+            ></Tab>
           </Tabs>
         </div>
       </div>
